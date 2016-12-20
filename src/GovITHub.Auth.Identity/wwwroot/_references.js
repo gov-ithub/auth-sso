284717eaa0ce08053030d580548758fca124ee74
alexchiraples <alexandru.chiraples@ithub.gov.ro>
2016-12-20 14:10:56 +0200

    _references update

--- src/GovITHub.Auth.Identity/wwwroot/_references.js
+++ src/GovITHub.Auth.Identity/wwwroot/_references.js
@@ -1,4 +1,6 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="js/login.js" />
 /// <reference path="js/site.js" />
+/// <reference path="vendor/bootstrap/dist/js/bootstrap.js" />
+/// <reference path="vendor/jquery-validation/dist/jquery.validate.js" />
 /// <reference path="vendor/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
