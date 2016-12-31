64e63fce4c35c3eee6b3b0927bbaf3f12ee738c4
dmihaita <daniel.mihaita@gmail.com>
2016-12-31 07:53:55 +0200

    Use correct scope for app state

--- src/GovITHub.Auth.Admin/wwwroot/app/components/organizations/edit.controller.js
+++ src/GovITHub.Auth.Admin/wwwroot/app/components/organizations/edit.controller.js
@@ -15,10 +15,13 @@
             vm.id = id;
 
             var create = function () {
-                vm.data.parentOrganizationId = $scope.currentUser.organizationId;
+                if ($scope.currentUser.organizationId >= 1)
+                    vm.data.parentOrganizationId = $scope.currentUser.organizationId;
                 Organization
                     .save(vm.data).$promise
                     .then(function (result) {
+                        $scope.currentUser.organizationId = result.id;
+                        $scope.currentUser.organizationName = result.name;
                         $state.go('index.organizations');
                     }).catch(function (err) {
                         vm.error = err;
@@ -58,8 +61,6 @@
             if (vm.status.edit) {
                 vm.init();
             };
-
-
 
             // -->End
             $scope.$on('$destroy', function () {
