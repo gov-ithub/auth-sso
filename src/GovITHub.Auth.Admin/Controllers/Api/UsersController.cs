d12db1379c3a264b56e4846755c4a0b27de084ee
Alex Oancea <alx.oancea@gmail.com>
2017-01-18 22:26:25 +0200

    feat(organization-users): prototype CRUD Users - uncomment the AuthorizeAttribute as the authentication process works now #52

--- src/GovITHub.Auth.Admin/Controllers/Api/UsersController.cs
+++ src/GovITHub.Auth.Admin/Controllers/Api/UsersController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace GovITHub.Auth.Admin.Controllers.Api
 {
-    //[Authorize]
+    [Authorize]
     [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
